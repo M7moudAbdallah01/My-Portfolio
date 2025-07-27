db758ac16d171ba4f419a87902a45d1a291f46d6
M7moudAbdallah01 <mahmoudcs50course@gmail.com>
2025-07-28 00:58:58 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,20 @@
+const menuToggle = document.getElementById("menu-toggle");
+const nav = document.querySelector("nav");
+
+menuToggle.addEventListener("click", () => {
+  nav.classList.toggle("active");
+});
+
+document.querySelectorAll("nav a").forEach((link) => {
+  link.addEventListener("click", () => {
+    nav.classList.remove("active");
+  });
+});
+
 const backToTopButton = document.getElementById("Backtotop");
 
 window.addEventListener("scroll", () => {
-  if (document.documentElement.scrollTop > 200) {
+  if (window.pageYOffset > 300) {
     backToTopButton.style.display = "block";
   } else {
     backToTopButton.style.display = "none";
@@ -9,21 +22,5 @@ window.addEventListener("scroll", () => {
 });
 
 backToTopButton.addEventListener("click", () => {
-  window.scrollTo({
-    top: 0,
-    behavior: "smooth"
-  });
-});
-
-const menuToggle = document.getElementById("menu-toggle");
-const nav = document.querySelector("nav");
-
-menuToggle.addEventListener("click", () => {
-    nav.classList.toggle("active");
-});
-
-document.querySelectorAll("nav a").forEach(link => {
-    link.addEventListener("click", () => {
-        nav.classList.remove("active");
-    });
+  window.scrollTo({ top: 0, behavior: "smooth" });
 });
