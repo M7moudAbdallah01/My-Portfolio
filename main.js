ff8f3c4c0fc2ab1aa60fcaa1da0d1b8c55df59a0
M7moudAbdallah01 <mahmoudcs50course@gmail.com>
2025-08-24 03:52:45 +0300

    121234

--- main.js
+++ main.js
@@ -24,3 +24,53 @@ window.addEventListener("scroll", () => {
 backToTopButton.addEventListener("click", () => {
   window.scrollTo({ top: 0, behavior: "smooth" });
 });
+
+        const toggleBtn = document.getElementById("theme-toggle");
+        const body = document.body;
+
+        if(localStorage.getItem("theme") === "dark"){
+            body.classList.add("dark-mode");
+            toggleBtn.innerHTML = '<i class="fa-solid fa-sun"></i>';
+        }
+
+        toggleBtn.addEventListener("click", () => {
+            body.classList.toggle("dark-mode");
+            if(body.classList.contains("dark-mode")){
+                toggleBtn.innerHTML = '<i class="fa-solid fa-sun"></i>';
+                localStorage.setItem("theme", "dark");
+            } else {
+                toggleBtn.innerHTML = '<i class="fa-solid fa-moon"></i>';
+                localStorage.setItem("theme", "light");
+            }
+        });
+
+
+const typingElement = document.getElementById("typing");
+const words = [ "Mahmoud Abdallah " , "Aspiring Penetration Tester "];
+let wordIndex = 0;
+let charIndex = 0;
+let isDeleting = false;
+
+function typeEffect() {
+  let currentWord = words[wordIndex];
+  
+  if (isDeleting) {
+    typingElement.textContent = currentWord.substring(0, charIndex--);
+  } else {
+    typingElement.textContent = currentWord.substring(0, charIndex++);
+  }
+
+  if (!isDeleting && charIndex === currentWord.length) {
+    isDeleting = true;
+    setTimeout(typeEffect, 1400);  
+    return;
+  } else if (isDeleting && charIndex === 0) {
+    isDeleting = false;
+    wordIndex = (wordIndex + 1) % words.length;
+  }
+
+  let speed = isDeleting ? 100 : 150;
+  setTimeout(typeEffect, speed);
+}
+
+typeEffect();
