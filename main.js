273b92539408271f597ea6b79c310a6292f67355
M7moudAbdallah01 <mahmoudcs50course@gmail.com>
2026-09-17 16:00:24 +0300

    deploy

---
+++ main.js
@@ -0,0 +1,56 @@
+const menuToggle = document.getElementById("menu-toggle");
+const nav = document.querySelector("nav");
+
+menuToggle.addEventListener("click", () => {
+  nav.classList.toggle("active");
+});
+
+document.querySelectorAll("nav a").forEach((link) => {
+  link.addEventListener("click", () => {
+    nav.classList.remove("active");
+  });
+});
+
+const backToTopButton = document.getElementById("Backtotop");
+
+window.addEventListener("scroll", () => {
+  if (window.pageYOffset > 300) {
+    backToTopButton.style.display = "block";
+  } else {
+    backToTopButton.style.display = "none";
+  }
+});
+
+backToTopButton.addEventListener("click", () => {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+});
+
+const typingElement = document.getElementById("typing");
+const words = [ "Mahmoud Abdallah " , "Jr. Penetration Tester "];
+let wordIndex = 0;
+let charIndex = 0;
+let isDeleting = false;
+
+function typeEffect() {
+  let currentWord = words[wordIndex];
+  
+  if (isDeleting) {
+    typingElement.innerHTML = currentWord.substring(0, charIndex--);
+  } else {
+    typingElement.innerHTML = currentWord.substring(0, charIndex++);
+  }
+
+  if (!isDeleting && charIndex === currentWord.length) {
+    isDeleting = true;
+    setTimeout(typeEffect, 1400);  
+    return;
+  } else if (isDeleting && charIndex === 0) {
+    isDeleting = false;
+    wordIndex = (wordIndex + 1) % words.length;
+  }
+
+  let speed = isDeleting ? 100 : 150;
+  setTimeout(typeEffect, speed);
+}
+
+typeEffect();
